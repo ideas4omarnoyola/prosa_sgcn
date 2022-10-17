c1754445035aca81d9fcf67fd17e65c3127a04bf
Xania Mendoza <xania.mendoza@ideas4learning.com>
2022-10-17 10:29:36 -0500

    pagina 19 eliminada

--- js/paginas.js
+++ js/paginas.js
@@ -1,7 +1,7 @@
 const paginas = [];
 
 function generarPaginas(){
-    const totalPaginas = 65;
+    const totalPaginas = 66;
 
     for(let contador = 1; contador<=totalPaginas; contador++){
         let nombrePagina;
